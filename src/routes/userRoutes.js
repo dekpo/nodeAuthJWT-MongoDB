b407ababd19a1c447d385714ac141f65306b9b5a
Dekpo <dekpo@me.com>
2021-04-15 13:52:01 +0200

    Added routes logic

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const routes = express.Router();
+const controllers = require('../controllers/userControllers');
+const passport = require('passport');
+
+routes.post('/register', controllers.registerUser);
+routes.post('/login', controllers.loginUser);
+
+routes.get('/admin', passport.authenticate('jwt', { session: false}), (req,res)=>{
+    return res.json({'msg': 'Hello ! '+req.user.email+' Welcome to your admin panel !!!'});
+});
