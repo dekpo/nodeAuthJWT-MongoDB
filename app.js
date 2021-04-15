12517b8a17da04ec09029cb56df919562862eccb
Dekpo <dekpo@me.com>
2021-04-15 14:17:50 +0200

    Compact app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config');
-
 const app = express();
 // config express
 app.use(cors());
@@ -14,7 +13,6 @@ app.use(express.urlencoded({extended: true}));
 app.use(passport.initialize());
 const passportMiddleware = require('./src/middleware/passport');
 passport.use(passportMiddleware);
-
 // la route classique et les routes spécifiques
 app.get('/', (req,res)=>{
     res.send('The server is running on port '+PORT);
@@ -22,10 +20,7 @@ app.get('/', (req,res)=>{
 const routes = require('./src/routes/userRoutes');
 app.use('/', routes);
 app.listen(PORT);
-
 // on se connecte au serveur MongoDB
 mongoose.connect(config.db, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, () => {
     console.log('MongoDB connected successfully !!!');
 });
-
-
